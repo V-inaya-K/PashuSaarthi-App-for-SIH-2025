2f5ddcbc1b901c946697636d5196312a92fb43bd
Vinayak Shukla <98159594+V-inaya-K@users.noreply.github.com>
2025-09-05 16:20:44 +0530

    Update authkey.dart

--- lib/Authentication/authkey.dart
+++ lib/Authentication/authkey.dart
@@ -1,4 +1,4 @@
 // lib/supabase_keys.dart
 
-const String supabaseUrl = 'https://yolfjybhkgybuomnbgqu.supabase.co'; // Replace with your actual Supabase URL
-const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbGZqeWJoa2d5YnVvbW5iZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3MTcyOTUsImV4cCI6MjA2OTI5MzI5NX0.YTapxQrCY5BWRY5BINaEvwCxgJHZoTTzr-M6ttPBA6w'; // Replace with your actual anon key
+const String supabaseUrl = '{}'; // Replace with your actual Supabase URL
+const String supabaseAnonKey = '{}'; // Replace with your actual anon key
